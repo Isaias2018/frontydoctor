886c05215319e76d1da205275b2fb0d7da66daf0
Isaias Chamorro <42631531+Isaias2018@users.noreply.github.com>
2021-04-16 10:36:05 -0500

    con el dggillasdasdlst

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 
-//Install express server
-const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/doctor-fast'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/doctor-fast/index.html'));
+app.use(express.static(__dirname+'/dist/DoctorFast'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/DoctorFast/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
